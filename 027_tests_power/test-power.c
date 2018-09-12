aa680e10e68042f7bdf015a556ae062f2e3a5c57
ym129 <ym129@duke.edu>
2018-09-11 20:05:29 -0400

    sub ans 1

---
+++ 027_tests_power/test-power.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+unsigned power(unsigned x, unsigned y);
+//void run_check(unsigned x, unsigned y, unsigned expected_ans);
+int main(void) {
+  //run_check(1, 1, 1);
+  if (power(1, 1) != 1) {
+    exit(EXIT_FAILURE);
+  }
+  if (power(0, 0) != 1) {
+    exit(EXIT_FAILURE);
+  }
+  if (power(1, -1) != 1) {
+    exit(EXIT_FAILURE);
+  }
+  /* if (power(2, 32) != 4294967296) { */
+  /*   exit(EXIT_FAILURE); */
+  /* } */
+  if (power(1, 2) != 1) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (power(1, 3) != 1) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (power(2, 1) != 2) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (power(2, 2) != 4) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (power(10, 9) != 1000000000) {
+    exit(EXIT_FAILURE);
+  }
+
+  return EXIT_SUCCESS;
+}
