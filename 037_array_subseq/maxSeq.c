00451b63e49bd7f87f61a55e506283ed3bad3891
ym129 <ym129@duke.edu>
2018-09-14 12:54:32 -0400

    sub ans 2

--- 037_array_subseq/maxSeq.c
+++ 037_array_subseq/maxSeq.c
@@ -3,6 +3,7 @@ size_t maxSeq(int * array, size_t n) {
   if (n == 0)
     return 0;
   size_t ans = 1;
+  size_t mx = 0;
   for (int i = 1; i < n; i++) {
     if (array[i] > array[i - 1]) {
       ans++;
@@ -11,7 +12,9 @@ size_t maxSeq(int * array, size_t n) {
       ans = 1;
       i++;
     }
+    if (ans > mx)
+      mx = ans;
   }
 
-  return ans;
+  return mx;
 }
