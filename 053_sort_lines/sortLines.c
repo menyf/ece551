66a73a41777ebfeb7138661c95854cd15a25a37e
ym129 <ym129@duke.edu>
2018-09-25 11:41:51 -0400

    sub ans 1

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 //This function is used to figure out the ordering
 //of the strings in qsort.  You do not need
 //to modify it.
@@ -15,10 +14,34 @@ int stringOrder(const void * vp1, const void * vp2) {
 void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
-
+void func(FILE * f) {
+  char * str = NULL;
+  size_t sz = 0;
+  int len = 0;
+  while ((len = getline(&str, &sz, f)) != EOF) {
+    sortData(&str, len);
+    printf("%s\n", str);
+  }
+  free(str);
+}
 int main(int argc, char ** argv) {
-  
   //WRITE YOUR CODE HERE!
-  
+  if (argc == 1) {
+    func(stdin);
+  }
+  else {
+    for (int i = 1; i < argc; i++) {
+      FILE * f = fopen(argv[i], "r");
+      if (f == NULL) {
+        perror("Can not find file");
+        exit(EXIT_FAILURE);
+      }
+      func(f);
+      if (fclose(f)) {
+        perror("Failed to close file");
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
   return EXIT_SUCCESS;
 }
