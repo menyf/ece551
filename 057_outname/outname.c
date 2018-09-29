bc4a5fbc93db45334d89e9bc39d1218eeba0ac74
ym129 <ym129@duke.edu>
2018-09-29 12:43:28 -0400

    sub ans 1

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,14 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  int len = strlen(inputName);
+  char * rt = malloc((len + 8) * sizeof(*rt));
+  strncpy(rt, inputName, len + 8);
+  strncpy(rt + len, ".counts", 8);
+  return rt;
 }
