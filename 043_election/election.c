ace9584265efcef66c314d7674a88c52fe0154e0
ym129 <ym129@duke.edu>
2018-09-29 00:54:55 -0400

    finish step 4, further test to be done

--- 043_election/election.c
+++ 043_election/election.c
@@ -1,11 +1,62 @@
 #include "election.h"
 
+#include "assert.h"
 #include "ctype.h"
 #include "stdint.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
 //include any other headers you need here...
+
+enum error_tag {
+  LEADING_COLON,
+  TAILING_COLON,
+  LESS_COLON,
+  MORE_COLON,
+  ADJACENT_COLONS,
+  WRONG_POPULATION_NUMBER,
+  WRONG_VOTE_NUMBER,
+  TOO_LONG_INPUT,
+  NO_ERROR
+};
+
+typedef enum error_tag error_t;
+
+void handle_error(error_t err, const char * line) {
+  if (err == LEADING_COLON) {
+    fprintf(stderr, "This line has at least one leading ':'(s) : %s\n", line);
+    exit(EXIT_FAILURE);
+  }
+  else if (err == TAILING_COLON) {
+    fprintf(stderr, "This line has at least one tailing ':'(s) : %s\n", line);
+    exit(EXIT_FAILURE);
+  }
+  else if (err == LESS_COLON) {
+    fprintf(stderr, "This line does not have enough ':'s : %s\n", line);
+    exit(EXIT_FAILURE);
+  }
+  else if (err == MORE_COLON) {
+    fprintf(stderr, "This line has too many ':'s : %s\n", line);
+    exit(EXIT_FAILURE);
+  }
+  else if (err == ADJACENT_COLONS) {
+    fprintf(stderr, "This line has adjacent ':'s : %s\n", line);
+    exit(EXIT_FAILURE);
+  }
+  else if (err == WRONG_POPULATION_NUMBER) {
+    fprintf(stderr, "There is some problem with the population number : %s\n", line);
+    exit(EXIT_FAILURE);
+  }
+  else if (err == WRONG_VOTE_NUMBER) {
+    fprintf(stderr, "There is some problem with the electoral vote number : %s\n", line);
+    exit(EXIT_FAILURE);
+  }
+  else if (err == TOO_LONG_INPUT) {
+    fprintf(stderr, "This line is too long : %s\n", line);
+    exit(EXIT_FAILURE);
+  }
+}
+
 void parse_state_name(char * dest, const char * src, const int len) {
   for (int i = 0; i < len; i++) {
     dest[i] = src[i];
@@ -29,19 +80,6 @@ unsigned parse_vote_number(const char * str, int l, int r) {
   return ans;
 }
 
-enum error_tag {
-  LEADING_COLON,
-  TAILING_COLON,
-  LESS_COLON,
-  MORE_COLON,
-  ADJACENT_COLONS,
-  WRONG_POPULATION_NUMBER,
-  WRONG_VOTE_NUMBER,
-  TOO_LONG_INPUT,
-  NO_ERROR
-};
-
-typedef enum error_tag error_t;
 state_t parseLine(const char * line) {
   //STEP 1: write me
   state_t state;
@@ -80,41 +118,11 @@ state_t parseLine(const char * line) {
   if (err == NO_ERROR && col2 == -1) {
     err = LESS_COLON;
   }
-  // error handling
-  if (err == LEADING_COLON) {
-    fprintf(stderr, "This line has at least one leading ':'(s) : %s\n", line);
-    exit(EXIT_FAILURE);
-  }
-  else if (err == TAILING_COLON) {
-    fprintf(stderr, "This line has at least one tailing ':'(s) : %s\n", line);
-    exit(EXIT_FAILURE);
-  }
-  else if (err == LESS_COLON) {
-    fprintf(stderr, "This line does not have enough ':'s : %s\n", line);
-    exit(EXIT_FAILURE);
-  }
-  else if (err == MORE_COLON) {
-    fprintf(stderr, "This line has too many ':'s : %s\n", line);
-    exit(EXIT_FAILURE);
-  }
-  else if (err == ADJACENT_COLONS) {
-    fprintf(stderr, "This line has adjacent ':'s : %s\n", line);
-    exit(EXIT_FAILURE);
-  }
-  else if (err == WRONG_POPULATION_NUMBER) {
-    fprintf(stderr, "There is some problem with the population number : %s\n", line);
-    exit(EXIT_FAILURE);
-  }
-  else if (err == WRONG_VOTE_NUMBER) {
-    fprintf(stderr, "There is some problem with the electoral vote number : %s\n", line);
-    exit(EXIT_FAILURE);
-  }
-  else if (err == TOO_LONG_INPUT) {
-    fprintf(stderr, "Thise line is too long : %s\n", line);
-    exit(EXIT_FAILURE);
-  }
 
-  // exclude all possible errors
+  // error handling
+  handle_error(err, line);
+
+  // Excluded all possible errors
   parse_state_name(state.name, line, col1);
   state.population = parse_population(line, col1 + 1, col2);
   state.electoralVotes = parse_vote_number(line, col2 + 1, ed);
@@ -123,14 +131,38 @@ state_t parseLine(const char * line) {
 
 unsigned int countElectoralVotes(state_t * stateData, uint64_t * voteCounts, size_t nStates) {
   //STEP 2: write me
-
-  return 0;
+  unsigned int ans = 0;
+  for (size_t i = 0; i < nStates; i++) {
+    if (voteCounts[i] > stateData[i].population / 2) {
+      ans += stateData[i].electoralVotes;
+    }
+  }
+  return ans;
 }
 
 void printRecounts(state_t * stateData, uint64_t * voteCounts, size_t nStates) {
   //STEP 3: write me
+  for (size_t i = 0; i < nStates; i++) {
+    uint64_t lower_bound = (long double)stateData[i].population * 0.495 + 1e-8;
+    uint64_t upper_bound = (long double)stateData[i].population * 0.505 + 1e-8;
+    if (lower_bound <= voteCounts[i] && voteCounts[i] <= upper_bound) {
+      printf("%s requires a recount (Candidate A has %.2f%% of the vote)\n",
+             stateData[i].name,
+             100.0f * voteCounts[i] / stateData[i].population);
+    }
+  }
 }
 
 void printLargestWin(state_t * stateData, uint64_t * voteCounts, size_t nStates) {
   //STEP 4: write me
+  double percentage = 0;
+  int idx = -1;
+  for (size_t i = 0; i < nStates; i++) {
+    long double curr_percentage = (long double)voteCounts[i] / stateData[i].population;
+    if (curr_percentage > percentage) {
+      percentage = curr_percentage;
+      idx = (int)i;
+    }
+  }
+  printf("Candidate A won %s with %.2f%% of the vote\n", stateData[idx].name, percentage * 100.0);
 }
