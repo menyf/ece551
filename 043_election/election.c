1396445c7899472e4e0f5514416d60bc00da8adf
ym129 <ym129@duke.edu>
2018-09-29 12:32:58 -0400

    assume the population number within uint_64, and votes with unsigned

--- 043_election/election.c
+++ 043_election/election.c
@@ -9,7 +9,7 @@
 //include any other headers you need here...
 
 enum error_tag {
-  LEADING_COLON,
+  //  LEADING_COLON,
   TAILING_COLON,
   LESS_COLON,
   MORE_COLON,
@@ -17,17 +17,19 @@ enum error_tag {
   WRONG_POPULATION_NUMBER,
   WRONG_VOTE_NUMBER,
   TOO_LONG_INPUT,
+  NEW_LINE_CONTAINED,
   NO_ERROR
 };
 
 typedef enum error_tag error_t;
 
 void handle_error(error_t err, const char * line) {
-  if (err == LEADING_COLON) {
-    fprintf(stderr, "This line has at least one leading ':'(s) : %s\n", line);
-    exit(EXIT_FAILURE);
-  }
-  else if (err == TAILING_COLON) {
+  /* if (err == LEADING_COLON) { */
+  /*   fprintf(stderr, "This line has at least one leading ':'(s) : %s\n", line); */
+  /*   exit(EXIT_FAILURE); */
+  /* } */
+  /* else */
+  if (err == TAILING_COLON) {
     fprintf(stderr, "This line has at least one tailing ':'(s) : %s\n", line);
     exit(EXIT_FAILURE);
   }
@@ -55,6 +57,26 @@ void handle_error(error_t err, const char * line) {
     fprintf(stderr, "This line is too long : %s\n", line);
     exit(EXIT_FAILURE);
   }
+  else if (err == NEW_LINE_CONTAINED) {
+    fprintf(stderr, "This input include '\n' character: %s\n", line);
+    exit(EXIT_FAILURE);
+  }
+}
+
+int compare_two_numbers(const char * num, int len_num, const char * maxValue) {
+  int len_max = strlen(maxValue);
+  if (len_num < len_max)
+    return 1;
+  if (len_num > len_max)
+    return 0;
+  for (int i = 0; i < len_num; i++) {
+    if (num[i] > maxValue[i]) {
+      return 0;
+    }
+    else if (num[i] < maxValue[i])
+      return 1;
+  }
+  return 1;  // equal
 }
 
 void parse_state_name(char * dest, const char * src, const int len) {
@@ -84,12 +106,16 @@ state_t parseLine(const char * line) {
   //STEP 1: write me
   state_t state;
   int col1 = -1, col2 = -1, ed = strlen(line);
-  error_t err = ed > 63 ? TOO_LONG_INPUT : NO_ERROR;
-  if (line[0] == ':') {
-    err = LEADING_COLON;
-  }
-  else if (line[ed - 1] == ':') {
+  error_t err = NO_ERROR;
+  /* if (line[0] == ':') { */
+  /*   err = LEADING_COLON; */
+  /* } */
+  /* else  */
+  if (line[ed - 1] == ':') {
     err = TAILING_COLON;
+  }
+  else if (strchr(line, '\n') != NULL) {
+    err = NEW_LINE_CONTAINED;
   }
   for (int i = 0; i < ed && err == NO_ERROR; i++) {
     if (i && line[i] == ':' && line[i - 1] == ':') {
@@ -98,6 +124,9 @@ state_t parseLine(const char * line) {
     if (line[i] == ':') {
       if (col1 == -1) {
         col1 = i;
+        if (col1 > MAX_STATE_NAME_LENGTH - 1) {
+          err = TOO_LONG_INPUT;
+        }
       }
       else if (col2 == -1) {
         col2 = i;
@@ -115,8 +144,16 @@ state_t parseLine(const char * line) {
       }
     }
   }
-  if (err == NO_ERROR && col2 == -1) {
-    err = LESS_COLON;
+  if (err == NO_ERROR) {
+    if (col2 == -1) {  // no or one ':' only.
+      err = LESS_COLON;
+    }
+    else if (!compare_two_numbers(line + col1 + 1, col2 - (col1 + 1), "18446744073709551615")) {
+      err = WRONG_POPULATION_NUMBER;
+    }
+    else if (!compare_two_numbers(line + col2 + 1, ed - (col2 + 1), "4294967295")) {
+      err = WRONG_VOTE_NUMBER;
+    }
   }
 
   // error handling
@@ -143,8 +180,8 @@ unsigned int countElectoralVotes(state_t * stateData, uint64_t * voteCounts, siz
 void printRecounts(state_t * stateData, uint64_t * voteCounts, size_t nStates) {
   //STEP 3: write me
   for (size_t i = 0; i < nStates; i++) {
-    uint64_t lower_bound = (long double)stateData[i].population * 0.495 + 1e-8;
-    uint64_t upper_bound = (long double)stateData[i].population * 0.505 + 1e-8;
+    uint64_t lower_bound = (long double)stateData[i].population * 0.495 + 0.5;
+    uint64_t upper_bound = (long double)stateData[i].population * 0.505 + 0.5;
     if (lower_bound <= voteCounts[i] && voteCounts[i] <= upper_bound) {
       printf("%s requires a recount (Candidate A has %.2f%% of the vote)\n",
              stateData[i].name,
