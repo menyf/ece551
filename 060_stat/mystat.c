19dd9a0c496a292dc7f7b1677823ba7d78ad8d5d
ym129 <ym129@duke.edu>
2018-10-08 21:18:26 -0400

    060 step 2 finished.

--- 060_stat/mystat.c
+++ 060_stat/mystat.c
@@ -10,10 +10,13 @@
 
 // Required header files
 #include <string.h>
-#include <sys/sysmacros.h>
 // Step 1
 char * fetchType(mode_t st_mode);
-void printInfo(const char * filename, struct stat * sb);
+void printGeneralInfo(const char * filename, struct stat * sb);
+
+// Step 2
+void printAccessInfo(struct stat * sb);
+char * proceedAccess(struct stat * sb);
 
 //This function is for Step 4
 char * time2str(const time_t * when, long ns);
@@ -31,8 +34,8 @@ int main(int argc, char ** argv) {
     exit(EXIT_FAILURE);
   }
 
-  printInfo(argv[1], &sb);
-
+  printGeneralInfo(argv[1], &sb);
+  printAccessInfo(&sb);
   return EXIT_SUCCESS;
 }
 
@@ -57,7 +60,7 @@ char * fetchType(mode_t st_mode) {
   }
 }
 
-void printInfo(const char * filename, struct stat * sb) {
+void printGeneralInfo(const char * filename, struct stat * sb) {
   char * filetype = fetchType(sb->st_mode);
 
   printf("  File: ‘%s’\n", filename);  // to be proceed.
@@ -73,6 +76,54 @@ void printInfo(const char * filename, struct stat * sb) {
          (long)sb->st_nlink);
 
   free(filetype);
+}
+
+char * proceedAccess(struct stat * sb) {
+  char * str = malloc(11 * sizeof(*str));
+
+  switch (sb->st_mode & S_IFMT) {
+    case S_IFBLK:
+      str[0] = 'b';
+      break;
+    case S_IFCHR:
+      str[0] = 'c';
+      break;
+    case S_IFDIR:
+      str[0] = 'd';
+      break;
+    case S_IFIFO:
+      str[0] = 'p';
+      break;
+    case S_IFLNK:
+      str[0] = 'l';
+      break;
+    case S_IFREG:
+      str[0] = '-';
+      break;
+    case S_IFSOCK:
+      str[0] = 's';
+      break;
+    default:
+      str[0] = '?';
+      break;
+  }
+
+  const mode_t mask[] = {
+      S_IRUSR, S_IWUSR, S_IXUSR, S_IRGRP, S_IWGRP, S_IXGRP, S_IROTH, S_IWOTH, S_IXOTH};
+  const char identifier[] = "rwx";
+
+  for (int i = 0; i < 9; i++) {
+    str[i + 1] = sb->st_mode & mask[i] ? identifier[i % 3] : '-';
+  }
+
+  str[10] = 0;
+  return str;
+}
+
+void printAccessInfo(struct stat * sb) {
+  char * access = proceedAccess(sb);
+  printf("Access: (%04o/%s)\n", sb->st_mode & ~S_IFMT, access);
+  free(access);
 }
 
 char * time2str(const time_t * when, long ns) {
