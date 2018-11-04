29ea294457446d32abe79ca8c77df94e655319ae
ym129 <ym129@duke.edu>
2018-11-04 11:06:53 -0500

    094 sub ans 3: resubmit the first answer

--- 094_binsrch/search.cpp
+++ 094_binsrch/search.cpp
@@ -1,13 +1,10 @@
-
-
 #include "function.h"
 
 int binarySearchForZero(Function<int, int> * f, int low, int high) {
-  if (low == high - 1)
+  if (low >= high - 1)
     return low;
   int mid = (low + high) / 2;
   int val = f->invoke(mid);
-
   if (val > 0)
     return binarySearchForZero(f, low, mid);
   else if (val < 0)
