74f22aca149c155fbfb3e2b6533460ae41171452
ym129 <ym129@duke.edu>
2018-11-04 11:04:07 -0500

    094 sub ans 02: test

--- 094_binsrch/search.cpp
+++ 094_binsrch/search.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
+
 
 #include "function.h"
 
 int binarySearchForZero(Function<int, int> * f, int low, int high) {
-  if (low >= high - 1)
+  if (low == high - 1)
     return low;
   int mid = (low + high) / 2;
   int val = f->invoke(mid);
-  //  std::cout << val << std::endl;
+
   if (val > 0)
     return binarySearchForZero(f, low, mid);
   else if (val < 0)
