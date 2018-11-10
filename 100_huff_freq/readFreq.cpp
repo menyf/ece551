23036e09ac33a48bfafed44820b26fd5fedbc7cb
ym129 <ym129@duke.edu>
2018-11-10 12:37:15 -0500

    100 sub ans 1

--- 100_huff_freq/readFreq.cpp
+++ 100_huff_freq/readFreq.cpp
@@ -1,9 +1,12 @@
-#include <stdio.h>
-#include <cstdlib>
 #include "readFreq.h"
 
+#include <stdio.h>
+
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
 void printSym(std::ostream & s, unsigned sym) {
-  if (sym > 256 ) {
+  if (sym > 256) {
     s << "INV";
   }
   else if (sym == 256) {
@@ -21,4 +24,13 @@ void printSym(std::ostream & s, unsigned sym) {
 }
 uint64_t * readFrequencies(const char * fname) {
   //WRITE ME!
+  uint64_t * ret = new uint64_t[257]();
+  std::ifstream ifs(fname);
+  char c = 0;
+  while (ifs.get(c)) {
+    ret[(unsigned char)c]++;
+  }
+  ret[256]++;
+  ifs.close();
+  return ret;
 }
