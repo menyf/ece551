90d965b0dd3c91baa6d00f54f8b181f4c762444a
ym129 <ym129@duke.edu>
2018-11-10 12:52:45 -0500

    101 sub ans 1

--- 101_huff_tree/buildTree.cpp
+++ 101_huff_tree/buildTree.cpp
@@ -1,5 +1,21 @@
 #include "node.h"
+#include "queue"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue_t pq;
+  for (int i = 0; i < 257; i++) {
+    if (counts[i])
+      pq.push(new Node(i, counts[i]));
+  }
+  while (pq.size() > 1) {
+    Node * a = pq.top();
+    pq.pop();
+    Node * b = pq.top();
+    pq.pop();
+    Node * c = new Node(a, b);
+    pq.push(c);
+  }
+
+  return pq.top();
+}
