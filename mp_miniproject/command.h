ec17657ab1d93839c2b81fed82f169d2c24e924f
ym129 <ym129@duke.edu>
2018-11-25 17:39:24 -0500

    mp: finished step2

--- mp_miniproject/command.h
+++ mp_miniproject/command.h
@@ -4,21 +4,29 @@
 
 #include <algorithm>
 #include <cstdio>
+#include <cstring>
 #include <iostream>
 #include <string>
+#include <vector>
+
 class Command
 {
  private:
   std::string command;
+  std::vector<std::string> args;
 
  public:
   Command() {}
-  Command(std::string command) : command(command) {}
-  Command(const Command & rhs) : command(rhs.command) {}
+  Command(std::string command) { parse(command); }
+  Command(const Command & rhs) : command(rhs.command), args(rhs.args) {}
   virtual ~Command() {}
   Command & operator=(const Command & rhs);
   Command & operator=(const std::string & rhs);
   void exec();
+  void parse(std::string cmd);
+  std::string parse_blank(std::string str);
+  std::string get_command() { return command; }
+  void set_command(const std::string str) { command = str; }
 };
 
 void Command::exec() {
@@ -33,11 +41,23 @@ void Command::exec() {
     exit(EXIT_FAILURE);
   }
 
-  if (cpid == 0) { /* Code executed by child */
-    char * newargv[] = {NULL};
-    char * newenviron[] = {NULL};
-    execve(command.c_str(), newargv, newenviron);
-    perror("execve"); /* execve() returns only on error */
+  if (cpid == 0) {                              /* Code executed by child */
+    if (access(command.c_str(), F_OK) == -1) {  // cannot find it.
+      std::cout << "Command " << command << " not found\n";
+    }
+    else {
+      char ** newargv = new char *[args.size() + 2];
+      newargv[0] = (char *)command.c_str();
+      for (size_t i = 0; i < args.size(); i++) {
+        newargv[i + 1] = (char *)args[i].c_str();
+      }
+      newargv[args.size() + 1] = NULL;
+
+      char * newenviron[] = {NULL};
+      execve(newargv[0], newargv, newenviron);
+      perror("execve"); /* execve() returns only on error */
+      delete[] newargv;
+    }
   }
   else { /* Code executed by parent */
     w = waitpid(cpid, &wstatus, WUNTRACED | WCONTINUED);
@@ -65,12 +85,77 @@ Command & Command::operator=(const Command & rhs) {
   if (this != &rhs) {
     Command temp(rhs);
     std::swap(temp.command, command);
+    std::swap(temp.args, args);
   }
   return *this;
 }
 
 Command & Command::operator=(const std::string & rhs) {
-  Command temp(rhs);
-  (*this) = temp;
+  parse(rhs);
   return *this;
+}
+
+void Command::parse(std::string cmd) {
+  std::vector<std::string> strs;
+  std::string formatted_cmd;
+
+  // Step1:
+  // (a) trim leading spaces,
+  // (b) continuous spaces between args
+  // (c) trim tailing spaces
+
+  /* We use i to iterate the whole string
+   * we use j to indicate the index of the last meaningful char
+   */
+  int j = -1;
+  for (size_t i = 0; i < cmd.size(); i++) {
+    if (cmd[i] == ' ' && j == -1) {  // leading space
+      continue;
+    }
+    else if (cmd[i] == ' ' && cmd[i - 1] != '\\') {
+      if ((int)i == j + 1) {
+        formatted_cmd += cmd[i];
+      }
+    }
+    else {
+      formatted_cmd += cmd[i];
+      j = i;
+    }
+  }
+  int len_formatted_cmd = formatted_cmd.length();
+  // tailing space
+  if (formatted_cmd[len_formatted_cmd - 1] == ' ' && formatted_cmd[len_formatted_cmd - 2] != '\\')
+    formatted_cmd = formatted_cmd.substr(0, len_formatted_cmd - 1);
+
+  //  std::cout << "After the first trim: '" << formatted_cmd << "'\n";
+
+  // Step2: divide string according to space
+  size_t l = 0;
+  for (size_t i = 0; i < formatted_cmd.length(); i++) {
+    if (formatted_cmd[i] == ' ' && formatted_cmd[i - 1] != '\\') {
+      std::string sub_str = formatted_cmd.substr(l, i - l);
+      strs.push_back(parse_blank(sub_str));
+      l = i + 1;
+    }
+  }
+  strs.push_back(parse_blank(formatted_cmd.substr(l)));
+
+  // Step3: extract values
+  command = strs[0];
+  strs.erase(strs.begin());
+  args = strs;
+}
+
+std::string Command::parse_blank(std::string str) {
+  std::string res;
+  for (size_t i = 0; i < str.length(); i++) {
+    if (str[i] == '\\' && i + 1 < str.length() && str[i + 1] == ' ') {
+      res += ' ';
+      i++;
+    }
+    else {
+      res += str[i];
+    }
+  }
+  return res;
 }
--- mp_miniproject/shell.h
+++ mp_miniproject/shell.h
@@ -1,19 +1,36 @@
+#include <unistd.h>
+
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "command.h"
 
 class Shell
 {
  private:
-  //vector<command> commands;
+  //vector<Command> commands;
   Command command;
+  std::vector<std::string> path;
 
  public:
-  Shell() {}
+  Shell();
   void run();
+  void complete_command(std::string _command);
 };
 
+Shell::Shell() {
+  // Initialize PATH
+  char * _path = getenv("PATH");
+  char * pch;
+  pch = strtok(_path, ":");
+  while (pch != NULL) {
+    path.push_back(pch);
+    pch = strtok(NULL, ":");
+  }
+}
+
+// The entry of Shell
 void Shell::run() {
   std::string binary_filename;
   std::cout << "myShell$ ";
@@ -22,7 +39,20 @@ void Shell::run() {
       break;
     }
     command = binary_filename;
+    complete_command(command.get_command());
     command.exec();
     std::cout << "myShell$ ";
+  }
+}
+
+void Shell::complete_command(std::string _command) {
+  if (_command.find("/") == std::string::npos) {
+    for (size_t i = 0; i < path.size(); i++) {
+      std::string full_path = path[i] + "/" + _command;
+      if (access(full_path.c_str(), F_OK) != -1) {
+        command.set_command(full_path);
+        return;
+      }
+    }
   }
 }
